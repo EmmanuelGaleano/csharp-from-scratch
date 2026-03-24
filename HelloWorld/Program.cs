38f60b1ebdd206f4481adf093a99aee338a73401
Emmanuel Galeano <emmanuelgaleano013@gmail.com>
2026-03-23 21:29:40 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program{
+        static void Main(){
+            var Name = "jhon";
+            Name = "1jhon";
+            Console.WriteLine($"Hello, World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
